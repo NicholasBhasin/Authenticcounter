59169f212bdaa1dd814f5729e5e44d02891a64f7
NicholasBhasin <87204019+NicholasBhasin@users.noreply.github.com>
2022-05-12 07:08:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var i = 0; 
+
+function counting()
+{
+    document.getElementById("countingLabel").value = ++i;
+}
+
+
+function subtract() 
+{
+    document.getElementById("countingLabel").value = --i;
+}
